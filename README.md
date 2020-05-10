4d2561757218fd0f29f3522bb886318d6a4183ed
Larry Hill <tomella1@gmail.com>
2020-05-10 17:06:08 +0930

    Update to cater for newer node and gulp. No functional change

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # elvis-positioning
 GDA94 to GDA2020 data transformation
+
+Preparing to develop
+
+* git clone https://github.com/Tomella/elvis-positioning.git 
+* npm install
+* bower install
+
+That's assuming you already have a working node version and development environment. The latest incarnation is built against 12.x of node. If not then [get node](https://nodejs.org/en/download/) and installlbower with `npm install -g bower`
+
+You will get warnings about bower being deprecated but remember this app was written years ago. Feel free to update the packaging to something newer.
+
+Once you are sure you have everything to run OK there are some system variables needed to run the 
+* PORT optional port to run the server on. The default is 3000
+* ESRI_USERNAME Used to get a token for calling the FME service that does the transformation. If not set it will try and proxy it but it probably isn't going to be that reliable.
+* ESRI_PASSWORD As above.
+
+Now we are ready to run from the base directory of the project.
+1. `gulp` Get the automated build happening.
+2. `node server` Get the server running
+3. [Open the page](http://localhost:3000)
+4. Happy code changes. Refresh the browser on updates and all should be good.
+
+I expect that if you are developing against this code base then I have probably left. You have probably cloned the project somewhere that you can make updates. If you have done that don't forget to update the deployment script to match under `code_deploy/static_deploy`
+
+Last thing. The server does not run in production. The code is static. The services are provided through the fsdf-elevation project. Again, if you want to redo it, feel free. 
--- gulpfile.js
+++ gulpfile.js
@@ -1,130 +1,134 @@
 // Include gulp
-var gulp = require('gulp');
+let {series, parallel, watch, src, dest} = require('gulp');
+
 
 // Include Our Plugins
-var fs            = require('fs');
-var header        = require('gulp-header');
-var eslint        = require('gulp-eslint');
-var babel         = require('gulp-babel');
-var concat        = require('gulp-concat');
-var concatCss     = require('gulp-concat-css');
-var insert        = require('gulp-insert');
-var uglify        = require('gulp-uglify');
-var rename        = require('gulp-rename');
-var templateCache = require('gulp-angular-templatecache');
-var addStream     = require('add-stream');
+let eslint = require('gulp-eslint');
+let fs = require('fs');
+let header = require('gulp-header');
+let babel = require('gulp-babel');
+let concat = require('gulp-concat');
+let concatCss = require('gulp-concat-css');
+let uglify = require('gulp-uglify');
+let templateCache = require('gulp-angular-templatecache');
+let addStream = require('add-stream');
 
-var directories = {
-	assets:      'dist/positioning/assets',
-	source:      'source',
-	resources:  'resources',
-	outresources:'dist/positioning/resources',
-   views:      'views',
-   outbower:   'dist/positioning/bower_components'
+let directories = {
+   assets: 'dist/positioning/assets',
+   source: 'source',
+   resources: 'resources',
+   outresources: 'dist/positioning/resources',
+   views: 'views',
+   outbower: 'dist/positioning/bower_components'
 };
 
 // Lint Task
-gulp.task('lint', function() {
-    return gulp.src(directories.source + '/**/*.js')
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-});
+function lint() {
+   return src(directories.source + '/**/*.js')
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+}
+exports.lint = lint;
 
-gulp.task('resources', function () {
-    return gulp.src(directories.resources + '/**/*')
-        .pipe(gulp.dest(directories.outresources));
-});
+function resources() {
+   return src(directories.resources + '/**/*')
+      .pipe(dest(directories.outresources));
+}
+exports.resources = resources;
 
-gulp.task('views', function () {
-    return gulp.src(directories.views + '/**/*')
-        .pipe(gulp.dest('dist'));
-});
+function views() {
+   return src(directories.views + '/**/*')
+      .pipe(dest('dist'))
+}
+exports.views = views;
 
 //Concatenate & Minify JS
-gulp.task('scripts', ["commonScripts", 'positioningScripts']);
-
-//Concatenate & Minify JS
-gulp.task('commonScripts', function() {
+function commonScripts() {
    return prepareScripts('common');
-});
-
-gulp.task('positioningScripts', function() {
-   return prepareScripts('positioning');
-});
-
-function prepareScripts(name) {
-   return gulp.src(directories.source + '/' + name + '/**/*.js')
-      .pipe(babel({
-            compact: false,
-            comments: true,
-            presets: ['es2015'],
-            plugins: ["syntax-async-generators", "transform-regenerator"]
-      }))
-	   .pipe(addStream.obj(prepareNamedTemplates(name)))
-      .pipe(concat(name + '.js'))
-      .pipe(header(fs.readFileSync(directories.source + '/licence.txt', 'utf8')))
-      .pipe(gulp.dest(directories.assets));
 }
+exports.commonScripts = commonScripts;
+
+function positioningScripts() {
+   return prepareScripts('positioning');
+}
+exports.positioningScripts = positioningScripts;
 
 
 //Concatenate & Minify JS
-gulp.task('squash', []); // ['squashCommon','squashIcsm', 'squashWater', 'squashStart', 'squashImagery', 'squashPositioning']);
-
-gulp.task('squashCommon', function() {
-	return gulp.src(directories.assets + '/common.js')
-      .pipe(babel({
-            compact: true,
-            comments:	false,
-            presets: ['es2015'],
-            plugins: ["syntax-async-generators"]
-      }))
-		.pipe(uglify())
-      .pipe(header(fs.readFileSync(directories.source + '/licence.txt', 'utf8')))
-		.pipe(gulp.dest(directories.assets + "/min"));
-});
-
-gulp.task('squashPositioning', function() {
-	return squashJs('positioning');
-});
-
-function squashJs(name) {
-	return gulp.src(directories.assets + '/' + name + '.js')
-		.pipe(uglify())
-		.pipe(gulp.dest(directories.assets + "/min"));
+function squash(cb) {
+   cb();
 }
+exports.squash = squash; // ['squashCommon','squashIcsm', 'squashWater', 'squashStart', 'squashImagery', 'squashPositioning']);
 
+function squashCommon() {
+   return src(directories.assets + '/common.js')
+      .pipe(babel({
+         compact: true,
+         comments: false,
+         presets: ['es2015'],
+         plugins: ["syntax-async-generators"]
+      }))
+      .pipe(uglify())
+      .pipe(header(fs.readFileSync(directories.source + '/licence.txt', 'utf8')))
+      .pipe(dest(directories.assets + "/min"));
+}
+exports.squashCommon = squashCommon;
+
+function squashPositioning() {
+   return squashJs('positioning')
+}
+exports.squashPositioning = squashPositioning;
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-	// We watch both JS and HTML files.
-    gulp.watch(directories.source + '/**/*(*.js|*.html)', ['lint']);
-    gulp.watch(directories.source + '/common/**/*(*.js|*.html)', ['commonScripts']);
-    gulp.watch(directories.source + '/positioning/**/*(*.js|*.html)', ['positioningScripts']);
-    gulp.watch(directories.source + '/**/*.css', ['concatCss']);
-    gulp.watch(directories.assets + '/common.js', ['squashCommon']);
-    gulp.watch(directories.views +  '/*', ['views']);
-    gulp.watch(directories.resources + '/**/*', ['resources']);
-    //gulp.watch('scss/*.scss', ['sass']);
-});
+function watchFiles() {
+   let ignore = { ignoreInitial: false };
+   // We watch both JS and HTML files.
+   watch(directories.source + '/**/*(*.js|*.html)', ignore, lint);
+   watch(directories.source + '/common/**/*(*.js|*.html)', ignore, series(commonScripts, squashCommon));
+   watch(directories.source + '/positioning/**/*(*.js|*.html)', ignore, series(positioningScripts, squashPositioning));
+   watch(directories.source + '/**/*.css', ignore, catCss);
+//   watch(directories.assets + '/common.js', ignore, squashCommon);
+   watch(directories.views + '/*', ignore, views);
+   watch(directories.resources + '/**/*', ignore, resources);
+}
+exports.default = series(resources, views, package, watchFiles);
 
-gulp.task('concatCss', function () {
-  return gulp.src(directories.source + '/**/*.css')
-    .pipe(concatCss("positioning.css"))
-    .pipe(gulp.dest(directories.assets));
-});
+function catCss() {
+   return src(directories.source + '/**/*.css')
+      .pipe(concatCss("positioning.css"))
+      .pipe(dest(directories.assets));
+}
+exports.catCss = catCss;
 
-gulp.task('package', function() {
-   return gulp.src('package.json')
-      .pipe(gulp.dest(directories.assets));
-});
+function package() {
+   return src('package.json')
+      .pipe(dest(directories.assets));
+}
+exports.package = package;
 
-gulp.task('build', ['views', 'package', 'scripts', 'concatCss', 'resources'])
+// Private functions
+function squashJs(name) {
+   return src(directories.assets + '/' + name + '.js')
+      .pipe(uglify())
+      .pipe(dest(directories.assets + "/min"));
+}
 
-// Default Task
-gulp.task('default', ['lint', 'scripts', 'concatCss', 'watch', 'package', 'resources', 'views']);
+function prepareScripts(name) {
+   return src(directories.source + '/' + name + '/**/*.js')
+      .pipe(babel({
+         compact: false,
+         comments: true,
+         presets: ['es2015'],
+         plugins: ["syntax-async-generators", "transform-regenerator"]
+      }))
+      .pipe(addStream.obj(prepareNamedTemplates(name)))
+      .pipe(concat(name + '.js'))
+      .pipe(header(fs.readFileSync(directories.source + '/licence.txt', 'utf8')))
+      .pipe(dest(directories.assets));
+}
 
 function prepareNamedTemplates(name) {
-   return gulp.src(directories.source + '/' + name + '/**/*.html')
-      .pipe(templateCache({module: name + ".templates", root:name, standalone : true}));
+   return src(directories.source + '/' + name + '/**/*.html')
+      .pipe(templateCache({ module: name + ".templates", root: name, standalone: true }));
 }
